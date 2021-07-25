243efa38e8ae21ae5dfcd53920ae9aca105e99d2
StoleYourFridge <stoleyourfridge@gmail.com>
2021-07-26 00:54:23 +0300

    View function added

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -5,6 +5,27 @@
 
 using namespace std;
 
+struct Queue
+{
+    int count;
+    Queue* next;
+};
+
+void View(Queue* first)
+{
+    if (first == NULL)
+    {
+        cout << "Queue is empty, goodbye" << endl;
+        return;
+    }
+    cout << "Your queue : ";
+    while (first != NULL)
+    {
+        cout << first->count << " ";
+        first = first->next;
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!" << endl;
